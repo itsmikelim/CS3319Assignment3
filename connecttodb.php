ba75839f0b7bac9e34b8f05b35599a5d94d98c79
Mike Lim <mlim59@uwo.ca>
2018-12-03 18:20:06 -0500

    fdjk

--- connecttodb.php
+++ connecttodb.php
@@ -1,9 +1,9 @@
 <?php
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpass = "root";
+$dbpass = "mikelim";
 $dbname = "lreid2assign2db";
-$connection = mysqli_connecct($dbhost, $dbuser, $dbpass, $dbname);
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
     die("Database connection failed :") .
     mysql_connect_error() . " (" . mysql_connect_errno() . ")" ;
