5a06105f6d8937edaf2e7b0094aaec27c4caaf9d
Mike Lim <mlim59@uwo.ca>
2018-12-03 12:21:45 -0500

    1st commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>assignment3</title>
+</head>
+<body>
+
+<?php
+    include "connecttodb.php";
+?>
+
+<h1>Customers</h1>
+Select your customer:
+<form action="" method="post">
+<select name="pickacustomer" id="pickacustomer">
+    <option value="1">Select Here</option>
+
+<?php
+    include "getCustomer.php";
+?>
+</select>
+</form>
+<hr>
+<?php
+    if(isset ($_POST['pickacustomer'])) {
+        include "connecttodb.php";
+        include "getartwork.php";
+    }
+?>
+<hr>
+</body>
+<html>
