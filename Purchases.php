6fa02473bbda90909d796c3fc5f85837ba2ecf61
Mike Lim <mlim59@uwo.ca>
2018-12-03 23:36:18 -0500

    fdjk

--- Purchases.php
+++ Purchases.php
@@ -4,19 +4,19 @@
     $whichOrder = $_POST["pickanorder"];
     if ($whichDesc = 2) {
         if ($whichOrder = 2) {
-            $query = "CREATE VIEW purchasesView AS SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity WHERE product.prodID = purchases.prodID ORDER BY product.description ASC;";
+            $query = "SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity FROM product, purchases WHERE product.prodID = purchases.prodID ORDER BY product.description ASC;";
         }
         if($whichOrder = 3) {
-            $query = "CREATE VIEW purchasesView AS SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity WHERE product.prodID = purchases.prodID ORDER BY product.description DESC;";
+            $query = "SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity FROM product, purchases WHERE product.prodID = purchases.prodID ORDER BY product.description DESC;";
         }
     }
 
     if ($whichDesc = 3) {
         if ($whichOrder = 2) {
-            $query = "CREATE VIEW purchasesView AS SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity WHERE product.prodID = purchases.prodID ORDER BY product.cost ASC;";
+            $query = "SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity FROM product, purchases WHERE product.prodID = purchases.prodID ORDER BY product.cost ASC;";
         }
         if($whichOrder = 3) {
-            $query = "CREATE VIEW purchasesView AS SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity WHERE product.prodID = purchases.prodID ORDER BY product.cost DESC;";
+            $query = "SELECT product.description, product.cost, purchases.cusID, purchases.prodID,  purchases.Quantity FROM product, purchases WHERE product.prodID = purchases.prodID ORDER BY product.cost DESC;";
         }
     }
     
